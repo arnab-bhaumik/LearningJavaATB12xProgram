4901562849e3bc99b60b23b893852dfa6edc6b2f
ArnabB98 <bhaumikarnab5@gmail.com>
2025-05-17 19:54:35 +0530

    17th May

--- src/Tasks/Task3.java
+++ src/Tasks/Task3.java
@@ -2,9 +2,9 @@ package Tasks;
 
 public class Task3 {
     public static void main(String[] args) {
-        int N1 = 10;
-        int N2 = 20;
-        int N3 = 30;
+        int N1 = 20;
+        int N2 = 30;
+        int N3 = 40;
 
         int largest = N1>N2 ? ((N1>N3)? N1 : N3) : ((N2>N3)? N2 : N3);
         System.out.println(largest);
